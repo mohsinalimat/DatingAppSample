1185540dd4a55d519584f7e2a7e8bd8100d078fc
Abhishek Thapliyal <abhishekthapliyal7579@gmail.com>
2016-12-29 16:25:57 +0530

    File push #1

---
+++ TrulyMadlyDemo/TrulyMadlyDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrulyMadlyDemo
+//
+//  Created by Abhishek Thapliyal on 12/29/16.
+//  Copyright © 2016 Abhishek Thapliyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
